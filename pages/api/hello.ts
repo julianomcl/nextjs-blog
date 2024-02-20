1d61cfb7f3d2432bff2272fa301876080622dcfd
Juliano Maia <leite.juliano@gmail.com>
2024-02-20 17:14:29 -0300

    fix tests

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
-  console.log('hello world')
+  console.log('hello world 2')
 
   res.status(200).json({ text: 'Hello' })
 }
